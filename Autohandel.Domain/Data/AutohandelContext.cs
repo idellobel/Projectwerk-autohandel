087303ba4e284f11a1d4f4f5f13ad46397aa9a3f
idellobel <ivan.dellobel@gmail.com>
2018-02-22 12:35:03 +0100

    Opzetten solution

---
+++ Autohandel.Domain/Data/AutohandelContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autohandel.Domain.Data
+{
+    public class AutohandelContext : DbContext
+    {
+        //Om straks met behulp van Dependency Injection de Context configureerbaar te maken moet je een speciale constructor definiëren.
+        public AutohandelContext(DbContextOptions<AutohandelContext> options) : base(options)
+        { }
+
+        //Voeg de DbSet<T> collecties van de gewenste Entity klassen toe aan je Context.
+        //    Dit worden in essentie de tabellen in de databank.
+    }
+}
---
+++ Autohandel.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Autohandel.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
