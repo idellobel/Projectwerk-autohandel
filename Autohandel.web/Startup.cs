1c7079cb9e04bdeec73b7982ef20d1ac6ba6374c
idellobel <ivan.dellobel@gmail.com>
2020-04-13 17:59:27 +0200

    security-alerts

--- Autohandel.web/Startup.cs
+++ Autohandel.web/Startup.cs
@@ -49,8 +49,7 @@ namespace Autohandel.web
 
             services.AddAuthentication().AddGoogle(googleOptions =>
             {
-               
-
+                
             });
 
             services.AddAuthorization(options =>
